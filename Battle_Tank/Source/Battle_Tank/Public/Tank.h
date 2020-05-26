24a751875b03c246aa65c6624bde6ca4bf1ad09d
Aaron Mejia <aarmeji77@gmail.com>
2020-05-26 10:28:56 -0700

    Health Bar Start

--- Battle_Tank/Source/Battle_Tank/Public/Tank.h
+++ Battle_Tank/Source/Battle_Tank/Public/Tank.h
@@ -22,4 +22,8 @@ private:
 public:
 	// Called by engine when actor damage is dealt
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
+
+	// Returns current health as a percentage of starting health, between 0 & 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
 };
